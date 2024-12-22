fc3f11076925b23d97c5f8c367c482344755f3af
Brandon High <759848+highb@users.noreply.github.com>
2024-12-22 15:21:45 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# remarkable-templates
+
+My templates and a little script to help manage them on the Remarkable.
+
+## Setup
+
+Run `mise install` with [Mise](https://mise.jdx.dev/)
+
+## Usage
+
+Go to `Settings/About/Copyrights and licences` and find the IP address at the bottom.
+
+Then run `./manage.sh` to Backup/Restore/Sync/SSH into the Remarkable using that IP.
---
+++ manage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ -z $(which gum) ]]; then
+    echo "gum not installed. Run mise install"
+    exit 1
+fi
+
+set -e
+
+TEMPLATES_DIR="/usr/share/remarkable/templates"
+REMARKABLE_IP=$(gum input --value "10.0.40.109" --placeholder "Remarkable IP")
+REMARKABLE_USERHOST="root@${REMARKABLE_IP}"
+TASK=$(gum choose "backup" "restore" "sync" "ssh")
+
+if [[ "${TASK}" = "sync" ]]; then
+    echo "Syncing template images to ${REMARKABLE_USERHOST}"
+    scp noteto-*.png "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"
+    echo "Copying remarkable templates.json to workdir/templates.json"
+    scp "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"/templates.json workdir/templates.json
+    echo "Merging repo templates into workdir/templates.json.tmp"
+    # Merge .templates keys from repo into remarkable templates.json from add-templates.json
+    # if a name key already exists in remarkable templates.json, the value is overwritten
+    # if a name key does not exist in remarkable templates.json, it is added
+    # all other keys in remarkable templates.json are preserved, in their original order
+    jq --ascii-output -s '.[0] as $repo | .[1] as $remarkable | $repo | reduce keys[] as $key ({}; .[$key] = $repo[$key] + ($remarkable | .[$key]))' add-templates.json workdir/templates.json > workdir/templates.json.tmp
+    echo "Copying workdir/templates.json.tmp to remarkable"
+    scp workdir/templates.json.tmp "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"/templates.json
+    echo "Templates synced"
+elif [[ "${TASK}" = "backup" ]]; then
+    echo "Backing up templates from ${REMARKABLE_USERHOST}"
+    scp "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"/templates.json backup/templates.json
+    scp "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"/*.png backup/
+    scp "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"/*.svg backup/
+    echo "Templates backed up"
+elif [[ "${TASK}" = "restore" ]]; then
+    echo "Restoring templates to ${REMARKABLE_USERHOST}"
+    scp backup/templates.json "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"/templates.json
+    scp backup/*.png "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"
+    scp backup/*.svg "${REMARKABLE_USERHOST}":"${TEMPLATES_DIR}"
+    echo "Templates restored"
+elif [[ "${TASK}" = "ssh" ]]; then
+    echo "Remember: Templates are in ${TEMPLATES_DIR}"
+    ssh "${REMARKABLE_USERHOST}"
+fi
